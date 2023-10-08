4ae2717eaf557dfef5f8c605830513309c842915
Elizabeth <elizabeth.muthoni@student.moringaschool.com>
2023-10-08 09:49:36 +0300

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form=document.querySelector('form')
+  form.addEventListener('submit',(e)=>{
+      e.preventDefault()
+      buildTask(e.target.newtask.value)
+      form.reset()
+  let form =document.querySelector('form')
+  form.addEventListener('submit',(event)=>{
+    event.preventDefault()
+   // console.log(event.target.newtask.value)
+   // console.log(event.target.user.value)
+    let newTodo=event.target.newtask.value
+    let user=event.target.user.value
+    let priority=event.target.priority.value
+    console.log(priority)
+    Todo(newTodo,user,priority)
+    console.log(newTodo,user)
+    form.reset()
+  
+  })
+})
 });
